aebb6ffac3c0b07feedc7f917aa2fba217244980
SeoYangHun <syh8088@gmail.com>
2024-04-03 16:12:21 +0900

    폼 인증 - formLogin()
    폼 인증 - formLogin()

--- springsecurity-6.x/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ springsecurity-6.x/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,15 +1,24 @@
 package io.security.springsecuritymaster;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -20,7 +29,30 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(
+                        form -> form
+//                                .loginPage("/loginPage")
+                                .loginProcessingUrl("/loginProc")
+                                .defaultSuccessUrl("/", false)
+                                .failureUrl("/failed")
+                                .usernameParameter("userId")
+                                .passwordParameter("passwd")
+                                .successHandler(new AuthenticationSuccessHandler() {
+                                    @Override
+                                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                        System.out.println("authentication: " + authentication);
+                                        response.sendRedirect("/home");
+                                    }
+                                })
+                                .failureHandler(new AuthenticationFailureHandler() {
+                                    @Override
+                                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                        System.out.println("exception: " + exception.getMessage());
+                                        response.sendRedirect("/login");
+                                    }
+                                })
+                                .permitAll()
+                );
 
         return http.build();
     }
