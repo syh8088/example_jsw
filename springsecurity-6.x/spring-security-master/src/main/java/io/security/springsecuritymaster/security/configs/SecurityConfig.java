3118b8080fd46f52c9f10d31a18393fe1b607d24
SeoYangHun <syh8088@gmail.com>
2024-05-02 00:33:52 +0900

    커스텀 인증실패 핸들러 - AuthenticationFailureHandler
    커스텀 인증실패 핸들러 - AuthenticationFailureHandler

--- springsecurity-6.x/spring-security-master/src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
+++ springsecurity-6.x/spring-security-master/src/main/java/io/security/springsecuritymaster/security/configs/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
@@ -28,19 +29,21 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
     private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-                        .requestMatchers("/", "/signup").permitAll()
+                        .requestMatchers("/", "/signup", "/login*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(
                         form -> form.loginPage("/login").permitAll()
                                 .authenticationDetailsSource(authenticationDetailsSource)
                                 .successHandler(successHandler)
+                                .failureHandler(failureHandler)
                 )
 //                .userDetailsService(userDetailsService)
                 .authenticationProvider(authenticationProvider)
