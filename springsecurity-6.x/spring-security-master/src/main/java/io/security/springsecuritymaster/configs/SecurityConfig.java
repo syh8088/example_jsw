beb8cc681aaa5d4d12f26602ab56765214818d7f
SeoYangHun <syh8088@gmail.com>
2024-05-01 11:49:26 +0900

    회원가입 / PasswordEncoder
    회원가입 / PasswordEncoder

--- springsecurity-6.x/spring-security-master/src/main/java/io/security/springsecuritymaster/configs/SecurityConfig.java
+++ springsecurity-6.x/spring-security-master/src/main/java/io/security/springsecuritymaster/configs/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,17 +22,22 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated())
-
+                        .requestMatchers("/", "/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .formLogin(form -> form.loginPage("/login").permitAll())
         ;
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-        return  new InMemoryUserDetailsManager(user);
+        return new InMemoryUserDetailsManager(user);
     }
 }
---
+++ springsecurity-6.x/spring-security-master/src/main/java/io/security/springsecuritymaster/domain/entity/Account.java
@@ -0,0 +1,20 @@
+package io.security.springsecuritymaster.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "z_test_jsw_springsecurity_6.x_account")
+public class Account implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String roles;
+    private int age;
+}
