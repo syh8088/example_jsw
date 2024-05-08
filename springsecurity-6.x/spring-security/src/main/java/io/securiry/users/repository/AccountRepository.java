fd89288a3096b7345799fc42c94418415028f9c8
SeoYangHun <syh8088@gmail.com>
2024-05-08 21:07:49 +0900

    필요없는 프로젝트 삭제
    필요없는 프로젝트 삭제

--- springsecurity-6.x/spring-security/src/main/java/io/securiry/users/repository/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package io.securiry.users.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
--- springsecurity-6.x/spring-security/src/main/java/io/securiry/users/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package io.securiry.users.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Account, Long> {
-}
